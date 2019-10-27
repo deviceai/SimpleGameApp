2fb4a5f42d2b04550ba14e2bc43f39c1ebb2ff6c
deviceai <device.ai@gmail.com>
2019-10-27 19:21:16 +0200

    created start screen, button styles

--- app/src/main/java/gameapp/simplegameapp/MainActivity.java
+++ app/src/main/java/gameapp/simplegameapp/MainActivity.java
@@ -3,6 +3,8 @@ package gameapp.simplegameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }

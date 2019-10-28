605e7be8a274c597af2b3ac1a48e5aeaed79e241
deviceai <device.ai@gmail.com>
2019-10-28 23:26:36 +0200

    added method for 'back' button

--- app/src/main/java/gameapp/simplegameapp/GameLevels.java
+++ app/src/main/java/gameapp/simplegameapp/GameLevels.java
@@ -1,8 +1,11 @@
 package gameapp.simplegameapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +18,34 @@ public class GameLevels extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = findViewById(R.id.button_back);
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (Exception e){
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        catch (Exception e){
+
+        }
+
     }
 }

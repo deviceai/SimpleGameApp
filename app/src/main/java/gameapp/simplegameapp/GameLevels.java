d7e39ac6f7e3f4ae198fd1f9b4beed7e854b766f
deviceai <device.ai@gmail.com>
2019-10-28 23:02:59 +0200

    created gamelevels screen

---
+++ app/src/main/java/gameapp/simplegameapp/GameLevels.java
@@ -0,0 +1,19 @@
+package gameapp.simplegameapp;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
--- app/src/main/java/gameapp/simplegameapp/MainActivity.java
+++ app/src/main/java/gameapp/simplegameapp/MainActivity.java
@@ -2,9 +2,12 @@ package gameapp.simplegameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    Intent intent = new Intent(MainActivity.this, GameLevels.class);
+                    startActivity(intent);
+                    finish();
+
+                } catch (Exception e){
+
+                }
+            }
+        });
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);

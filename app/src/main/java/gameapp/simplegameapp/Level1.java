aa114dd0978509b8ba2c0c5214d3432d38a60e79
deviceai <device.ai@gmail.com>
2019-10-30 22:56:39 +0200

    added image containers for level1

--- app/src/main/java/gameapp/simplegameapp/Level1.java
+++ app/src/main/java/gameapp/simplegameapp/Level1.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView img_left = findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+        final ImageView img_right = findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
